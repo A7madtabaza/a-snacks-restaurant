b2099fa1bd3390b9dbab8acb7bd4cb1f7653d5df
A7madtabaza <ahmadtabaza272@gmail.com>
2024-12-29 20:41:45 +0300

    task

---
+++ task.js
@@ -0,0 +1,75 @@
+// var name1 = prompt("Enter his/her name");
+// var gender=prompt( "enter his/her gender  (male or female)");
+
+// if(gender==="male"){
+//     alert("name mr:" + name1);
+// }else if (gender==="female") {
+//   alert("name ms:"+ name1);
+// } else {
+//     alert("welcome");
+// }
+
+// var a="eshawarma"
+// var s="zinger"
+// var d="burger"
+// var order = prompt(" write the name of the ord eshawarma, zinger, or burger. ");
+ 
+// if(order===a)
+// {
+//     alert(`ypur order is ${a}`)
+// }
+// else if(order===s)
+// {
+//     alert(`ypur order is ${s}`);
+
+// }else{
+//      alert(`ypur order is ${d}`);
+// }
+
+// console.log(` ${name1}  your order is ${order}`)
+
+
+//Q2-------------------------------------------------------------------
+let numbers = parseInt(prompt("Please enter a number:"))
+switch (numbers) {
+  case 1:
+    console.log("ONE");
+    break;
+  case 2:
+    console.log("TWO");
+    break;
+  case 3:
+    console.log("THREE");
+    break;
+  case 4:
+    console.log("FOUR");
+    break;
+  case 5:
+    console.log("FIVE");
+    break;
+  case 6:
+    console.log("SIX");
+    break;
+  case 7:
+    console.log("SEVEN");
+    break;
+  case 8:
+    console.log("EIGHT");
+    break;
+  case 9:
+    console.log("NINE");
+    break;
+
+
+  default: 
+ console.log("AGAINE");
+    break;
+}
+
+
+
+
+
+
+
+
